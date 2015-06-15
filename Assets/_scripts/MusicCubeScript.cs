9e61bca7bf55b17bb41112634fdf6ef699714f66
JessRudder <thejessicarudder@gmail.com>
2015-06-14 21:39:40 -0400

    Add speed variable for music cube and expose it to editor

--- Assets/_scripts/MusicCubeScript.cs
+++ Assets/_scripts/MusicCubeScript.cs
@@ -2,15 +2,17 @@
 using System.Collections;
 
 public class MusicCubeScript : MonoBehaviour {
+	[SerializeField] public int speed;
 
 	// Use this for initialization
 	void Start () {
+		speed = 1;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (-Vector3.right * Time.deltaTime);
+		transform.Translate (-Vector3.right * Time.deltaTime * speed);
 	
 	}
 }
