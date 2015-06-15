186401cdc7ea3ac1b4ac3e8dc89f1e2efa7a14be
JessRudder <thejessicarudder@gmail.com>
2015-06-14 20:45:26 -0400

    Create initial test in C#

---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject musicCube;
+	public int killList;
+	public GameObject[] killObjects;
+	public GameObject killObject;
+	private int cubeCount;
+	public int killNum;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 50; i++) {
+			Instantiate (musicCube);
+		}
+		StartCoroutine ("killBox");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Destroy (GameObject.FindGameObjectWithTag ("box"));
+	}
+
+	IEnumerator killBox() {
+		cubeCount = GameObject.FindGameObjectsWithTag("box").Length;
+		yield return new WaitForSeconds (1f);
+		//Debug.Log(GameObject.FindGameObjectsWithTag("box").Length);
+		for (int i = 0; i < cubeCount - 1; i++) {
+			killObjects = GameObject.FindGameObjectsWithTag("box");
+			killList = GameObject.FindGameObjectsWithTag("box").Length;
+			killNum = Random.Range(0, killList - 1);
+			//Debug.Log(killNum);
+			killObject = killObjects[killNum];
+			Debug.Log("Before: " + killObjects[killNum] + " Length: " + killList);
+			Destroy (killObject);
+			yield return new WaitForSeconds (0.5f);
+			Debug.Log("After: " + killObjects[killNum] + " Length: " + killList);
+
+		}
+		yield return null;
+	}
+}
---
+++ Assets/_scripts/GameManager02.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager02 : MonoBehaviour {
+	public GameObject musicCube;
+	
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; i++) {
+			Instantiate(musicCube, new Vector3((5+(i*2)), 0.5f, 0), Quaternion.identity);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
